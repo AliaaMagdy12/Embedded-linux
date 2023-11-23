c4b67aa33da4807399bcc94ad1e1f2dddd0669a7
aliaa <aliaa0magdy722@gmail.com>
2023-11-23 11:32:00 +0200

    first commit

---
+++ task1_adminlinux/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir /home/ubuntu/mydirectory
+mkdir /home/ubuntu/mydirectory/seconddirectory
+touch /home/ubuntu/mydirectory/seconddirectory/mynotepaper
+cp /home/ubuntu/mydirectory/seconddirectory/mynotepaper /home/ubuntu/mydirectory/mynotepaper
+mv /home/ubuntu/mydirectory/mynotepaper /home/ubuntu/mydirectory/myoldnotepaper
+ 
+
