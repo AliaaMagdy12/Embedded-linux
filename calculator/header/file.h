f67a208949f39142e9c1dbb21534e9c9ac8c360a
aliaa <aliaa0magdy722@gmail.com>
2023-12-22 07:39:54 +0200

    first calc

---
+++ calculator/header/file.h
@@ -0,0 +1,5 @@
+float add(float num1, float num2);
+float subtract(float num1,float num2);
+float mult(float num1, float num2);
+float divide(float dividend, float divisor);
+//float Mod(float x, float y);
